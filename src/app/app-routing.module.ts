acd9a21510529719ec84cc708d80e2727209d365
Julian Cruz <juliand.cruzc@uqvirtual.edu.co>
2024-02-12 21:44:09 -0500

    Cambios Varios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,22 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './pagina/inicio/inicio.component';
 import { LoginComponent } from './pagina/login/login.component';
 import { RegistroComponent } from './pagina/registro/registro.component';
-import {RegistroMedicosComponent} from "./pagina/registro-medicos/registro-medicos.component";
+import { RegistroMedicosComponent} from "./pagina/registro-medicos/registro-medicos.component";
 import { ModificarPacienteComponent } from './pagina/modificar-paciente/modificar-paciente.component';
 import { LoginGuard } from './guards/permiso.service';
 import { RolesGuard } from './guards/roles.service';
-import {GestionPqrsComponent} from "./pagina/gestion-pqrs/gestion-pqrs.component";
-import {CrearPqrsComponent} from "./pagina/crear-pqrs/crear-pqrs.component";
-import {AgendarCitaComponent} from "./pagina/agendar-cita/agendar-cita.component";
-import {DetallePqrsComponent} from "./pagina/detalle-pqrs/detalle-pqrs.component";
-import {HistorialCitasPacienteComponent} from "./pagina/historial-citas-paciente/historial-citas-paciente.component";
-import {DetalleCitaComponent} from "./pagina/detalle-cita/detalle-cita.component";
-import {AtenderCitaComponent} from "./pagina/atender-cita/atender-cita.component";
-import {DetalleCitaMedicoComponent} from "./pagina/detalle-cita-medico/detalle-cita-medico.component";
-import {DiaLibreComponent} from "./pagina/dia-libre/dia-libre.component";
-import {HistorialCitasComponent} from "./pagina/historial-citas/historial-citas.component";
-import {CambiarPasswordComponent} from "./pagina/cambiar-password/cambiar-password.component";
-import {OlvidarPasswordComponent} from "./pagina/olvidar-password/olvidar-password.component";
+import { GestionPqrsComponent} from "./pagina/gestion-pqrs/gestion-pqrs.component";
+import { CrearPqrsComponent} from "./pagina/crear-pqrs/crear-pqrs.component";
+import { AgendarCitaComponent} from "./pagina/agendar-cita/agendar-cita.component";
+import { DetallePqrsComponent} from "./pagina/detalle-pqrs/detalle-pqrs.component";
+import { HistorialCitasPacienteComponent} from "./pagina/historial-citas-paciente/historial-citas-paciente.component";
+import { DetalleCitaComponent} from "./pagina/detalle-cita/detalle-cita.component";
+import { AtenderCitaComponent} from "./pagina/atender-cita/atender-cita.component";
+import { DetalleCitaMedicoComponent} from "./pagina/detalle-cita-medico/detalle-cita-medico.component";
+import { DiaLibreComponent} from "./pagina/dia-libre/dia-libre.component";
+import { HistorialCitasComponent} from "./pagina/historial-citas/historial-citas.component";
+import { CambiarPasswordComponent} from "./pagina/cambiar-password/cambiar-password.component";
+import { OlvidarPasswordComponent} from "./pagina/olvidar-password/olvidar-password.component";
 
 
 const routes: Routes = [
@@ -36,10 +36,11 @@ const routes: Routes = [
   { path: "historial-citas", component: HistorialCitasComponent, canActivate:[RolesGuard], data: { expectedRole: ["medico"]}},
   { path: "detalle-cita/:codigo", component:DetalleCitaComponent, canActivate:[RolesGuard], data: { expectedRole: ["paciente"]}},
   { path: "atender-cita", component:AtenderCitaComponent,canActivate:[RolesGuard], data: { expectedRole: ["medico"]}},
-  {path: "detalle-cita-medico/:codigo", component:DetalleCitaMedicoComponent, canActivate:[RolesGuard], data: {expectedRole: ["medico"]}},
+  { path: "detalle-cita-medico/:codigo", component:DetalleCitaMedicoComponent, canActivate:[RolesGuard], data: {expectedRole: ["medico"]}},
   { path: "dia-libre", component:DiaLibreComponent, canActivate:[RolesGuard], data: {expectedRole: ["medico"]}},
   { path: "cambiar-password", component: CambiarPasswordComponent,canActivate:[RolesGuard], data: {expectedRole: ["paciente"]} },
   { path: "olvidar-password", component: OlvidarPasswordComponent},
+  { path: "", component: InicioComponent },
   { path: "**", pathMatch: "full", redirectTo: "" }
 ];
 
